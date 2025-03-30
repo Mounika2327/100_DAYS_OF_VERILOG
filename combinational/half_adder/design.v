3d15fe9e43e736a361d16f708a92b5bc60786af6
MANGARATNAM <mangaratnamkondapalli@gmail.com>
2025-03-30 20:31:12 +0530

    design.v

--- combinational/half_adder/design.v
+++ combinational/half_adder/design.v
@@ -1,7 +1,8 @@
 // Code your design here
-module half_adder(a,b,sum,carry);
-  input a,b;
-  output sum,carry;
-  assign sum = a^b;
-  assign carry = a&b;
+module half_adder(
+    input A, B,
+    output Sum, Carry
+);
+  assign Sum = A ^ B;        // Sum = A XOR B
+  assign Carry = A & B;      // Carry = A AND B
 endmodule
