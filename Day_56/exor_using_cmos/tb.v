cd7f6a4ff1dafe4c10e5efda614517a93aa6ba99
MANGARATNAM <mangaratnamkondapalli@gmail.com>
2025-07-16 12:07:22 +0530

    Create tb.v

---
+++ Day_56/exor_using_cmos/tb.v
@@ -0,0 +1,16 @@
+module tb;
+  reg a,b;
+  wire y;
+  wire k;
+  exor_gate w6 (.a(a),.b(b),.y(y));
+ 
+  initial begin
+    $monitor("a=%0b,b=%0b,y=%0b", a, b, y);
+
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $finish;
+    end
+endmodule
